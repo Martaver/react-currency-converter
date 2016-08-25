9c3dda8c009565ec6dcde809ff640cbc6b63a515
Piotr Witek <piotrek.witek@gmail.com>
2016-08-25 14:54:54 +0200

    added layout header to main layout

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,6 +1,7 @@
 import './main-layout.css!';
 import * as React from 'react';
 import { LayoutTopNav, LayoutTopNavItem } from './components/layout-top-nav';
+import { LayoutHeader } from './components/layout-header';
 
 export function MainLayout({children}) {
   return (
@@ -10,6 +11,8 @@ export function MainLayout({children}) {
         <LayoutTopNavItem text="Currency Converter" route="/currency-converter" />
         <LayoutTopNavItem text="About" route="/about" right />
       </LayoutTopNav>
+
+      <LayoutHeader text="Welcome" />
 
       <main className="o-container o-container--large u-pillar-box--small">
         {children}
