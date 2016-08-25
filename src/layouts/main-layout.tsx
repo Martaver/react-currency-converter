b2e7757205065b6adb55ebd0931cdfb778c8afbd
Piotr Witek <piotrek.witek@gmail.com>
2016-08-25 15:38:46 +0200

    changed main layout to component

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -2,21 +2,26 @@ import './main-layout.css!';
 import * as React from 'react';
 import { LayoutTopNav, LayoutTopNavItem } from './components/layout-top-nav';
 import { LayoutHeader } from './components/layout-header';
+import { LayoutMain } from './components/layout-main';
 
-export function MainLayout({children}) {
-  return (
-    <div className="c-text">
-      <LayoutTopNav>
-        <LayoutTopNavItem text="Home" route="/" primary />
-        <LayoutTopNavItem text="Currency Converter" route="/currency-converter" />
-        <LayoutTopNavItem text="About" route="/about" right />
-      </LayoutTopNav>
+export class MainLayout extends React.Component<{}, {}> {
+  render() {
+    const {children} = this.props;
 
-      <LayoutHeader text="Welcome" />
+    return (
+      <div className="c-text">
+        <LayoutTopNav>
+          <LayoutTopNavItem text="Home" route="/" primary />
+          <LayoutTopNavItem text="Currency Converter" route="/currency-converter" />
+          <LayoutTopNavItem text="About" route="/about" right />
+        </LayoutTopNav>
 
-      <main className="o-container o-container--large u-pillar-box--small">
-        {children}
-      </main>
-    </div>
-  );
+        <LayoutHeader text="Welcome" />
+
+        <LayoutMain>
+          {children}
+        </LayoutMain>
+      </div>
+    );
+  };
 };
