1c922ee9ad3ee990ab9a449af628e7934d925173
Piotr Witek <piotrek.witek@gmail.com>
2016-08-29 01:34:57 +0200

    added currency converter container

--- src/containers/currency-converter-container/index.tsx
+++ src/containers/currency-converter-container/index.tsx
@@ -1,18 +1,57 @@
 // lib imports
 import * as React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 // components imports
+import * as CurrencyRatesService from '../../services/fixer/currency-rates';
+import { CurrencyConverter } from './components/currency-converter';
 
 interface IProps {
+  currencyConverter: any;
+  currencyRates: any;
 }
 
 interface IState {
 }
 
 export class CurrencyConverterContainer extends React.Component<IProps, IState> {
+
+  updateBaseCurrency = (newCurrency: string) => {
+    console.log('update base currency');
+  }
+
+  updateTargetCurrency = (newCurrency: string) => {
+    console.log('update target currency');
+  }
+
   render() {
+    console.log(this.props);
+    if (this.props.currencyConverter == null) return null;
+    const { currencies, baseCurrency, targetCurrency, baseValue, targetValue } = this.props.currencyConverter;
+
     return (
-      <div className="">
-      </div>
+      <section className="">
+        <CurrencyConverter currencies={currencies}
+          baseCurrency={baseCurrency} targetCurrency={targetCurrency}
+          onBaseCurrencyChange={this.updateBaseCurrency}
+          onTargetCurrencyChange={this.updateTargetCurrency}
+          baseValue={baseValue} targetValue={targetValue} />
+      </section>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    currencyConverter: state.currencyConverter,
+    currencyRates: state.currencyRates
+  };
+}
+const actions = {};
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps)(CurrencyConverterContainer);
