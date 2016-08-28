f01660877698be6f1493af5105b26910cc61148e
Piotr Witek <piotrek.witek@gmail.com>
2016-08-28 18:33:09 +0200

    updated home and about pages

--- src/containers/home-container/index.tsx
+++ src/containers/home-container/index.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import { PageSection } from '../../components/page-section';
+import { PageHero } from '../../components/page-hero';
 
 export function HomeContainer() {
   return (
     <div>
-      <p>Just a placeholder right now for a real content.</p>
+      <PageHero title="Welcome" subtitle="React / Redux / TypeScript - starter-kit" />
+      <PageSection>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </PageSection>
     </div>
   );
 };
