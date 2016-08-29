6e48b0fa90aaf7b5c9ddb4c739f59b38c08b504f
Piotr Witek <piotrek.witek@gmail.com>
2016-08-29 09:47:47 +0200

    refactored reducers

--- src/reducers/currency-converter-reducer.tsx
+++ src/reducers/currency-converter-reducer.tsx
@@ -1,41 +1,44 @@
 import { createAction } from 'redux-actions';
 
-// Actions - LOAD, CREATE, UPDATE, REMOVE
+// Action Types - LOAD, CREATE, UPDATE, REMOVE
 export const UPDATE_BASE_CURRENCY = 'currencyConverter/UPDATE_BASE_CURRENCY';
 export const UPDATE_TARGET_CURRENCY = 'currencyConverter/UPDATE_TARGET_CURRENCY';
+export const UPDATE_BASE_VALUE = 'currencyConverter/UPDATE_BASE_VALUE';
+export const UPDATE_TARGET_VALUE = 'currencyConverter/UPDATE_TARGET_CURRENCY';
 
+// Action Creators
+export const updateBaseCurrency = createAction(UPDATE_BASE_CURRENCY);
+export const updateTargetCurrency = createAction(UPDATE_TARGET_CURRENCY);
+export const updateBaseValue = createAction(UPDATE_BASE_VALUE);
+export const updateTargetValue = createAction(UPDATE_TARGET_VALUE);
+
+// Reducer
 const defaultState = {
-  currencies: {
-    'PLN': 1,
-    'SEK': 1.4782
-  },
   baseCurrency: 'PLN',
   targetCurrency: 'SEK',
   baseValue: 100,
   targetValue: 100
 };
 
-// Reducer
 export default function reducer(state = defaultState, action: FluxStandardAction<any> = {}) {
   switch (action.type) {
     case UPDATE_BASE_CURRENCY:
-      return Object.assign(
-        {},
-        state,
-        { baseCurrency: action.payload }
-      );
+      return Object.assign({}, state, {
+        baseCurrency: action.payload
+      });
     case UPDATE_TARGET_CURRENCY:
-      return Object.assign(
-        {},
-        state,
-        { targetCurrency: action.payload }
-      );
+      return Object.assign({}, state, {
+        targetCurrency: action.payload
+      });
+    case UPDATE_BASE_VALUE:
+      return Object.assign({}, state, {
+        baseValue: action.payload
+      });
+    case UPDATE_TARGET_VALUE:
+      return Object.assign({}, state, {
+        targetValue: action.payload
+      });
 
     default: return state;
   }
 }
-
-// Action Creators
-export const updateBaseCurrency = createAction(UPDATE_BASE_CURRENCY);
-
-export const updateTargetCurrency = createAction(UPDATE_TARGET_CURRENCY);
--- src/reducers/currency-rates-reducer.tsx
+++ src/reducers/currency-rates-reducer.tsx
@@ -1,46 +1,46 @@
 import { createAction } from 'redux-actions';
 
-// Actions - LOAD, CREATE, UPDATE, REMOVE
+// Action Types - LOAD, CREATE, UPDATE, REMOVE
 export const LOAD_CURRENCY_RATES = 'currencyRates/LOAD';
 export const LOAD_CURRENCY_RATES_SUCCESS = 'currencyRates/LOAD_SUCCESS';
 export const LOAD_CURRENCY_RATES_ERROR = 'currencyRates/LOAD_ERROR';
 
+// Action Creators
+export const loadCurrencyRates = createAction(LOAD_CURRENCY_RATES);
+export const loadCurrencyRatesSuccess = createAction(LOAD_CURRENCY_RATES_SUCCESS);
+export const loadCurrencyRatesError = createAction(LOAD_CURRENCY_RATES_ERROR);
+
+// Reducer
+const rates = {
+  'PLN': 1,
+  'SEK': 1.4782
+};
 const defaultState = {
   isLoading: false,
   errorMessage: null,
-  results: null,
-  lastUpdated: null
+  lastUpdated: null,
+  currencies: Object.keys(rates)
 };
 
-// Reducer
 export default function reducer(state = defaultState, action: FluxStandardAction<any> = {}) {
   switch (action.type) {
     case LOAD_CURRENCY_RATES:
-      return Object.assign(
-        {},
-        state,
-        { isLoading: true }
-      );
+      return Object.assign({}, state, {
+        isLoading: true
+      });
     case LOAD_CURRENCY_RATES_SUCCESS:
-      return Object.assign(
-        {},
-        state,
-        { isLoading: false, errorMessage: null, results: action.payload, lastUpdated: Date.now() }
-      );
+      return Object.assign({}, state, {
+        isLoading: false,
+        errorMessage: null,
+        results: action.payload,
+        lastUpdated: Date.now()
+      });
     case LOAD_CURRENCY_RATES_ERROR:
-      return Object.assign(
-        {},
-        state,
-        { isLoading: false, errorMessage: action.payload }
-      );
+      return Object.assign({}, state, {
+        isLoading: false,
+        errorMessage: action.payload
+      });
 
     default: return state;
   }
 }
-
-// Action Creators
-export const loadCurrencyRates = createAction(LOAD_CURRENCY_RATES);
-
-export const loadCurrencyRatesSuccess = createAction(LOAD_CURRENCY_RATES_SUCCESS);
-
-export const loadCurrencyRatesError = createAction(LOAD_CURRENCY_RATES_ERROR);
